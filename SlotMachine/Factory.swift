1b0bfc5df81a16d7b1c0b5b69fbe6e8c03d36d19
16saadc <16saadc@gmail.com>
2015-03-19 19:38:15 -0400

    finishing up proj

---
+++ SlotMachine/Factory.swift
@@ -0,0 +1,9 @@
+//
+//  Factory.swift
+//  SlotMachine
+//
+//  Created by 16saadc on 3/7/15.
+//  Copyright (c) 2015 ChrisSaad. All rights reserved.
+//
+
+import Foundation
---
+++ SlotMachine/SlotBrain.swift
@@ -0,0 +1,9 @@
+//
+//  SlotBrain.swift
+//  SlotMachine
+//
+//  Created by 16saadc on 3/19/15.
+//  Copyright (c) 2015 ChrisSaad. All rights reserved.
+//
+
+import Foundation
